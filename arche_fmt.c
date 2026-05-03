c65b42ca0ce41f01d9a7694ff9de7ac20db12128
Curt Reyes <curtreyes00@yahoo.com>
2026-05-03 14:31:19 -0600

    more loop fmt

--- arche_fmt.c
+++ arche_fmt.c
@@ -50,9 +50,20 @@ int main(int argc, char *argv[]) {
 	ParseResult parse_result = parse_source(src);
 
 	if (parse_result.error_count > 0) {
-		for (size_t i = 0; i < parse_result.error_count; i++) {
-			fprintf(stderr, "[Line %d, Col %d] Error: %s\n", parse_result.errors[i].line, parse_result.errors[i].column,
-			        parse_result.errors[i].message);
+		fprintf(stderr, "%s:\n", filename);
+		int unique_errors = 1;
+		fprintf(stderr, "  [Line %d, Col %d] Error: %s\n", parse_result.errors[0].line, parse_result.errors[0].column,
+		        parse_result.errors[0].message);
+		for (size_t i = 1; i < parse_result.error_count && unique_errors < 5; i++) {
+			if (parse_result.errors[i].line != parse_result.errors[i-1].line ||
+			    parse_result.errors[i].column != parse_result.errors[i-1].column) {
+				fprintf(stderr, "  [Line %d, Col %d] Error: %s\n", parse_result.errors[i].line, parse_result.errors[i].column,
+				        parse_result.errors[i].message);
+				unique_errors++;
+			}
+		}
+		if (parse_result.error_count > unique_errors) {
+			fprintf(stderr, "  ... and %zu more errors\n", parse_result.error_count - unique_errors);
 		}
 		Program *prog = parse_result.ast;
 		parse_result_free(&parse_result);
